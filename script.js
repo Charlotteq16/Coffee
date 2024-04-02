810fc6e04a8515041d04cd53d6a33046734baace
Charlotteq16 <144183035+Charlotteq16@users.noreply.github.com>
2024-04-02 15:57:33 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+
+
+
+
+filterObjects("all");
+
+function filterObjects(c){
+    var x, i;
+
+    x =document.getElementsByClassName("box");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++){
+        removeClass (x[i], "show");
+        if(x[i].className.indexOf(c) > -1) addClass(x [i], "show")
+    }
+}
+
+function addClass(element, name){
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++){
+        if (arr1.indexOf(arr2[i]) == -1){
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+function removeClass(element, name){
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i= 0; i < arr2.length; i++){
+        while (arr1.indexOf(arr2[i]) > -1){
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
